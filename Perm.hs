397e53c7ca563a5bc035540c79eff4e3b0156036
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2011-01-08 19:23:54 +0100

    Documentation and naming.

--- Perm.hs
+++ Perm.hs
@@ -1,58 +1,74 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE ExistentialQuantification #-}
 
-module Perm where
+module Perm
+  ( Effects(..), length, sequence, swap, firsts, perms
 
-import Prelude
+  -- * Lifting computations
+  , once
+
+  ) where
+
+import Prelude hiding (length, sequence)
 import Control.Applicative
 import Data.Foldable
 
--- A list of effectful computations. Only the final result is visible in the type.
-data Linear p a where
-  Nil  :: a -> Linear p a
-  (:-) :: p b -> Linear p (b -> a) -> Linear p a
+-- | A chain of effectful @p@-computations with final result @a@. Individual
+-- computations have their own result types, which fit together in standard
+-- 'Applicative' fashion. Although these result types are existentially
+-- quantified, the computations can still be moved around within the list
+-- (e.g. 'swap', 'firsts'). This allows their permutations to be computed.
+data Effects p a where
+  Nil  :: a -> Effects p a
+  (:-) :: p b -> Effects p (b -> a) -> Effects p a
 
 infixr 5 :-
 
 -- | Map over the final result type.
-instance Functor (Linear p) where
+instance Functor (Effects p) where
   fmap f (Nil x) = Nil (f x)
   fmap f (p :- ps) = p :- fmap (fmap f) ps
 
--- 'pure' represents the empty list of computations while '<*>' acts like '++'.
-instance Applicative (Linear p) where
+-- | 'pure' represents the empty list of computations while '<*>' acts like
+-- '++'.
+instance Applicative (Effects p) where
   pure = Nil
   Nil g <*> y = fmap g y
   (f :- x) <*> y = f :- (flip <$> x <*> y)
 
 -- | Compute the length of a list of computations.
-len :: Linear p a -> Int
-len (Nil _)     = 0
-len (_ :- xs) = 1 + len xs
+length :: Effects p a -> Int
+length (Nil _)     = 0
+length (_ :- xs) = 1 + length xs
 
--- | Lift a parser, making sure it is run once in each permutation.
-once :: p a -> Linear p a
+-- | Lift a computation, making sure it is run once in each permutation.
+once :: p a -> Effects p a
 once p = p :- Nil id
 
--- | Execute the list of computations in order.
-flatten :: Applicative p => Linear p a -> p a
-flatten (Nil x) = pure x
-flatten (p :- ps) = (\x f -> f x) <$> p <*> flatten ps
+-- | Execute the computations in order.
+sequence :: Applicative p => Effects p a -> p a
+sequence (Nil x) = pure x
+sequence (p :- ps) = (\x f -> f x) <$> p <*> sequence ps
 
--- | Build a tree (using <|> for choice) of all permutations of the computations.
-perms :: Alternative p => Linear p a -> p a
+-- | Build a tree (using '<|>' for branching) of all permutations of the
+-- computations. The tree shape allows permutations to share common prefixes.
+-- This allows clever computations to quickly prune uninteresting branches of
+-- permutations.
+perms :: Alternative p => Effects p a -> p a
 perms (Nil x) = pure x
-perms ps = asum . map permTail . firsts $ ps
+perms ps      = asum . map permTail . firsts $ ps
   where
     permTail (p :- ps') = flip ($) <$> p <*> perms ps'
     permTail _          = undefined
 
--- | Extract each element once and make it the head of the list.
-firsts :: Linear p a -> [Linear p a]
+-- | Give each effect a chance to be the first effect in the chain, producing
+-- @n@ new chains where @n@ is the 'length' of the input chain. In each case
+-- the relative order of the effects is preserved with exception of the effect
+-- that was moved to the front.
+firsts :: Effects p a -> [Effects p a]
 firsts (Nil _) = []
 firsts (p :- ps) = (p :- ps) : map (\ps' -> swap (p :- ps')) (firsts ps)
 
 -- | Swaps the first two elements of the list, if they exist.
-swap :: Linear p a -> Linear p a
+swap :: Effects p a -> Effects p a
 swap (p0 :- p1 :- ps) = p1 :- p0 :- fmap flip ps
 swap ps = ps
